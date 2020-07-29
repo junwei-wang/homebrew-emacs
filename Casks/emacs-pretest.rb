c620d42f74ee75ca88bd077246fde9fdbcf84535
Junwei Wang <junwei.wang@cryptoexperts.com>
2020-07-29 10:40:41 +0200

    Add cask formula

---
+++ Casks/emacs-pretest.rb
@@ -0,0 +1,23 @@
+cask "emacs-pretest" do
+  version "27.0.91-1"
+  sha256 "4d3d11e1b5193da733963adcc4b275ff93b29538463fbc8ab1363fa65b6bfc4e"
+
+  url "https://emacsformacosx.com/emacs-builds/Emacs-pretest-#{version}-universal.dmg"
+  appcast "https://emacsformacosx.com/atom/pretest"
+  name "Emacs"
+  homepage "https://emacsformacosx.com/"
+
+  conflicts_with formula: "emacs"
+
+  app "Emacs.app"
+  binary "#{appdir}/Emacs.app/Contents/MacOS/Emacs", target: "emacs"
+  binary "#{appdir}/Emacs.app/Contents/MacOS/bin/ebrowse"
+  binary "#{appdir}/Emacs.app/Contents/MacOS/bin/emacsclient"
+  binary "#{appdir}/Emacs.app/Contents/MacOS/bin/etags"
+
+  zap trash: [
+    "~/Library/Caches/org.gnu.Emacs",
+    "~/Library/Preferences/org.gnu.Emacs.plist",
+    "~/Library/Saved Application State/org.gnu.Emacs.savedState",
+  ]
+end
